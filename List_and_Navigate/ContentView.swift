23f902313505c583297eb5669a0d67a70dd8139c
DevDataOrg <48391348+DevDataOrg@users.noreply.github.com>
2021-01-07 17:45:09 +0300

    Initial Commit

---
+++ List_and_Navigate/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  List_and_Navigate
+//
+//  Created by Artyom Romanchuk on 07.01.2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ List_and_Navigate/List_and_NavigateApp.swift
@@ -0,0 +1,17 @@
+//
+//  List_and_NavigateApp.swift
+//  List_and_Navigate
+//
+//  Created by Artyom Romanchuk on 07.01.2021.
+//
+
+import SwiftUI
+
+@main
+struct List_and_NavigateApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
